1a6a759cb1730398b9871ee3e58509baf0bb9549
yujie <1312964528@qq.com>
2022-10-14 15:31:35 +0800

    Third Commit!

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git01!");
         System.out.println("Hello Git02!");
+        System.out.println("Hello Git03!");
     }
 }
