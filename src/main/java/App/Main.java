ce2070c8beb4115bd9f5a33bbd3b845448eb1a79
yujie <1312964528@qq.com>
2022-10-14 15:22:17 +0800

    Second Commit!

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -2,6 +2,7 @@ package App;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git01!");
+        System.out.println("Hello Git02!");
     }
 }
